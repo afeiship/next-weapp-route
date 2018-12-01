75c20cabe30dd0d2563300506308cd11c9b409e3
afeiship <1290657123@qq.com>
2018-12-01 21:19:12 +0800

    refactor: core route api

--- dist/next-weapp-route.js
+++ dist/next-weapp-route.js
@@ -2,18 +2,39 @@
 
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
+  var nxWxpromisify = nx.wxPromisify || require('next-wx-promisify');
+  var wx = global.wx;
 
-  var NxWeappRoute = nx.declare('nx.WeappRoute', {
+  // import packages:
+  require('next-param');
+
+  var NxWeappRoute = nx.declare('nx.weapp.Route', {
     statics: {
-      moduleKey: 'index',
-    },
-    methods: {
-      back: function () { },
-      push: function () { },
-      replace: function () { },
-      to: function () { },
-      reLaunch: function () { },
-      switchTao: function () { }
+      route: function (inApi, inUrl, inData) {
+        var suffix = inData ? ('?' + nx.param(inData)) : '';
+        var urlData = inUrl ? { url: inUrl + suffix } : null;
+        return new Promise(function (resolve, reject) {
+          wx[inApi](
+            nx.mix(urlData, nxWxpromisify(resolve, reject))
+          );
+        });
+      },
+      back: function (inData) {
+        this.route('navigateBack', null, inData);
+      },
+      push: function (inUrl, inData) {
+        this.route('navigateTo', inUrl, inData);
+      },
+      replace: function (inUrl, inData) {
+        this.route('redirectTo', inUrl, inData);
+      },
+      reLaunch: function (inUrl, inData) {
+        this.route('reLaunch', inUrl, inData)
+      },
+      switchTab: function (inKey) {
+        var url = '/' + inKey;
+        this.route('switchTab', url);
+      }
     }
   });
 
