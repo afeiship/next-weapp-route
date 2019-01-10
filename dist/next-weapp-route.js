8b10157f9bf2a19deeabd6caa467aeaca0158f2e
afeiship <afeiship@gmail.com>
2019-01-10 12:55:35 +0800

    fix: reutrn promise

--- dist/next-weapp-route.js
+++ dist/next-weapp-route.js
@@ -1,5 +1,4 @@
-(function () {
-
+(function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
   var nxWxpromisify = nx.wxPromisify || require('next-wx-promisify');
@@ -8,39 +7,35 @@
   // import packages:
   require('next-param');
 
-  var NxWeappRoute = nx.declare('nx.weapp.Route', {
+  var NxWeappRoute = nx.declare('nx.WeappRoute', {
     statics: {
-      route: function (inApi, inUrl, inData) {
-        var suffix = inData ? ('?' + nx.param(inData)) : '';
+      route: function(inApi, inUrl, inData) {
+        var suffix = inData ? '?' + nx.param(inData) : '';
         var urlData = inUrl ? { url: inUrl + suffix } : null;
-        return new Promise(function (resolve, reject) {
-          wx[inApi](
-            nx.mix(urlData, nxWxpromisify(resolve, reject))
-          );
+        return new Promise(function(resolve, reject) {
+          wx[inApi](nx.mix(urlData, nxWxpromisify(resolve, reject)));
         });
       },
-      back: function (inData) {
-        this.route('navigateBack', null, inData);
+      back: function(inData) {
+        return this.route('navigateBack', null, inData);
       },
-      go: function (inUrl, inData) {
-        this.route('navigateTo', inUrl, inData);
+      go: function(inUrl, inData) {
+        return this.route('navigateTo', inUrl, inData);
       },
-      replace: function (inUrl, inData) {
-        this.route('redirectTo', inUrl, inData);
+      replace: function(inUrl, inData) {
+        return this.route('redirectTo', inUrl, inData);
       },
-      reLaunch: function (inUrl, inData) {
-        this.route('reLaunch', inUrl, inData)
+      reLaunch: function(inUrl, inData) {
+        return this.route('reLaunch', inUrl, inData);
       },
-      switchTab: function (inKey) {
+      switchTab: function(inKey) {
         var url = '/' + inKey;
-        this.route('switchTab', url);
+        return this.route('switchTab', url);
       }
     }
   });
 
-
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = NxWeappRoute;
   }
-
-}());
+})();
